ff160e3c206d6cfc7280d2975003c71c174f5b42
ChristyanSilva <79805986+ChristyanSilva@users.noreply.github.com>
2023-05-31 10:48:33 -0500

    Add files via upload

--- Logica/ServicioVehiculo.cs
+++ Logica/ServicioVehiculo.cs
@@ -1,86 +1,41 @@
-﻿using Entidad;
+﻿using Datos;
+using Entidad;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Datos;
-using System.Diagnostics.Contracts;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace Logica
 {
-    public class ServicioVehiculo : IServicio<Vehiculo>
+    public class ServicioVehiculo
     {
-        List<Vehiculo> vehiculos;
-        RepositorioVehicular repositorioVehicular;
-        public ServicioVehiculo()
+       
+
+        public bool Guardardb(Vehiculo v)
         {
-            repositorioVehicular = new RepositorioVehicular();
-            vehiculos = repositorioVehicular.obtener();
+            return RepositorioVehiculo.Guardardb(v);
         }
 
-        public string Borrar(Vehiculo vehiculo)
+        public DataTable ListarTodo()
         {
-            vehiculos.Remove(vehiculo);
-            repositorioVehicular.actualizar(vehiculos);
-            Refresh();
-            return $"el vehiculo se elimino correctamente con la placa {vehiculo.placa}";
-        }
-        void Refresh()
-        {
-            vehiculos = repositorioVehicular.obtener();
+            return RepositorioVehiculo.listar();
         }
 
-        public string Editar(Vehiculo oldVehiculo, Vehiculo UpdateVehiculo)
+        public Vehiculo Consultar(string marca, string placa)
         {
-            oldVehiculo.marca = UpdateVehiculo.marca;
-            oldVehiculo.placa = UpdateVehiculo.placa;
-            oldVehiculo.aniosdeUso = UpdateVehiculo.aniosdeUso;
-            oldVehiculo.tipoGasolina = UpdateVehiculo.tipoGasolina;
-            oldVehiculo.kilometraje = UpdateVehiculo.kilometraje;
-            oldVehiculo.estadodelVehiculo = UpdateVehiculo.estadodelVehiculo;
-            var estado = repositorioVehicular.actualizar(vehiculos);
-            Refresh();
-            return estado ? $"Se ha actualizado el vehiculo{UpdateVehiculo.placa}" :
-                $"ERROR al actualizar el vehiculo {UpdateVehiculo.placa}";
+            return RepositorioVehiculo.Consultar(marca, placa);
         }
 
-        public bool Existencia(Vehiculo vehiculo)
+        public static bool Actualizar(Vehiculo v)
         {
-            throw new NotImplementedException();
+            return RepositorioVehiculo.Actualizar(v);
         }
 
-        public List<Vehiculo> Obtener()
+        public static bool Eliminar(string placa)
         {
-            return vehiculos;
-        }
-
-        public Vehiculo BuscarporPlaca(string Placa)
-        {
-            foreach (var item in vehiculos)
-            {
-                if (item.placa == Placa)
-                {
-                    return item;
-                }
-            }
-            return null;
-        }
-
-        public string Guardar(Vehiculo vehiculo)
-        {
-            if (BuscarporPlaca(vehiculo.placa) != null)
-            {
-                return $"El vehiculo seleccionado existe con esta ID:  {vehiculo.placa}";
-            }
-            var estado = repositorioVehicular.Guardar(vehiculo);
-
-            Refresh();
-
-            return estado ? $"el vehiculo se agrego correctamente con la placa:  {vehiculo.placa}" :
-                                 $"ERROR al Guardar el vehiculo con la placa:  {vehiculo.placa}";
+            return RepositorioVehiculo.Eliminar(placa);
         }
     }
 }
